4b5477b5b01f8679dce751584b4552e1c1b086f3
RiceDaddy321 <0carlitos2@gmail.com>
2020-09-02 20:18:12 -0500

    I have added information about strings and how they can be manipulated through their methods

--- Learning/HelloWorld.py
+++ Learning/HelloWorld.py
@@ -1,1 +1,21 @@
-print('Hello World")
+message = 'Hello World'
+multilineMessage = """This is a multi-line
+example...bruh"""
+
+print(message)
+'''
+#print(message[0:5]) - starts at the first index
+and stops at, but does not include, the last index
+
+#message.count('(character or string)') - counts the amonunt of occurcences of the string/character
+
+#message.find('(character/string)') - looks for the first occurence of the given expression
+#IMPORTANT--if it doesn't find it then it outputs a -1
+
+#message.replace('(string to be replaced)', '(what to replace with)')
+#IMPORTANT--it doesn't actually alter the original string, instead it returns the new string
+
+'''
+
+
+print(multilineMessage)
