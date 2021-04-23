5cb520fba3450cd70f6b46f10050d3735cb6370a
RiceDaddy321 <0carlitos2@gmail.com>
2021-04-23 14:41:32 -0500

    Finished Data Visualization, then went and started on building a website using python and django

--- Python_Crash_Course/Data Visualization/countries.py
+++ Python_Crash_Course/Data Visualization/countries.py
@@ -1,7 +1,4 @@
 from pygal.maps.world import COUNTRIES
 
-def get_country_code(country_name):
-    """Return the pygal 2-digit country code for the given country."""
-
 for country_code in sorted(COUNTRIES.keys()):
     print(country_code, COUNTRIES[country_code])
